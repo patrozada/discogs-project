70cd263dc45c6ab7193471cd92676d0c021cf380
AzureAD\PatriciaRozada <patricia.rozada@ultimasoftware.com>
2021-08-24 16:55:29 +0200

    #Data- enable input and button to make search feature work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {findSearch} from './services/search';
 
 function App() {
+	let term;
+	const handleInputChange = (e) => {
+		term = e.target.value;
+	}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+				<label htmlFor="search">type in sth to find</label>
+				<input type="text" name="search" id="search" onBlur={handleInputChange}/>
+				<button onClick={()=>findSearch(term)}> Go </button>
+      </header> 
     </div>
   );
 }
